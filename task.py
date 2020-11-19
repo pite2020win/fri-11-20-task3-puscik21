0a198158ef4c9b112e1f139bc24cb5c43d02c51b
grzegorz.puscizna <grzesiekpuscizna@gmail.com>
2020-11-19 22:57:31 +0100

    A lot of not DRY at all code, actually working but lacks of div_matrix, rdiv and rsub

--- task.py
+++ task.py
@@ -8,11 +8,87 @@ class Matrix:
             self.rows.append(row)
 
     def __add__(self, other):
+        if isinstance(other, Matrix):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] + other.rows[i][j])
+                new_matrix.rows.append(row)
+            return new_matrix
+        elif isinstance(other, int):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] + other)
+                new_matrix.rows.append(row)
+            return new_matrix
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        if isinstance(other, Matrix):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] - other.rows[i][j])
+                new_matrix.rows.append(row)
+            return new_matrix
+        elif isinstance(other, int):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] - other)
+                new_matrix.rows.append(row)
+            return new_matrix
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] * other.rows[i][j])
+                new_matrix.rows.append(row)
+            return new_matrix
+        elif isinstance(other, int):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] * other)
+                new_matrix.rows.append(row)
+            return new_matrix
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, int):
+            new_matrix = Matrix()
+            for i in range(len(self.rows)):
+                row = []
+                for j in range(len(self.rows[i])):
+                    row.append(self.rows[i][j] / other)
+                new_matrix.rows.append(row)
+            return new_matrix
+
+    def __matmul__(self, other):
         new_matrix = Matrix()
         for i in range(len(self.rows)):
             row = []
             for j in range(len(self.rows[i])):
-                row.append(self.rows[i][j] + other.rows[i][j])
+                mul_sum = 0
+                for k in range(len(self.rows[i])):
+                    mul_sum += self.rows[i][k] * other.rows[k][j]
+                row.append(mul_sum)
             new_matrix.rows.append(row)
         return new_matrix
 
@@ -28,59 +104,15 @@ if __name__ == '__main__':
     matrix1 = Matrix([[1, 2],
                       [3, 4]])
     matrix2 = Matrix([[2, 4],
-                     [6, 8]])
+                      [6, 8]])
 
-    print_matrix("matrix1", matrix1)
-    print_matrix("matrix2", matrix2)
-
-    matrix3 = matrix1 + matrix2
+    matrix3 = 10 - matrix1
     print_matrix("matrix3", matrix3)
 
-# @ - dot product
+    # arr1 = np.array([[1, 2],
+    #                  [3, 4]])
+    # arr2 = np.array([[1, 2],
+    #                  [3, 4]])
+    # arr1 = arr2 / arr1
+    # print_matrix("arr1", arr1)
 
-# arr1 = np.array([[1, 2],
-#                  [3, 4]])
-# arr2 = np.array([[2, 4],
-#                  [6, 8]])
-#
-# arr3 = arr1 * arr2
-# arr4 = np.dot(arr1, arr2)
-# print(arr1)
-# print(arr2)
-# print("arr3:\n{}".format(arr3))
-# print("arr4:\n{}".format(arr4))
-
-# Matrix.
-
-
-# Write a class that can represent any 4𝑥4 real matrix.
-# Include two functions to calculate the sum and dot product of two matrices.
-# Next, write a program that imports this library module and use it to perform calculations.
-# You CAN'T use numpy.
-# Examples:
-#
-# matrix_1 = Matrix(4.,5.,6.,7.)
-# matrix_2 = Matrix(2.,2.,2.,1.)
-#
-# matrix_3 = matrix_2 @ matrix_1
-# matrix_4 = matrix_2 + matrix_1
-# matrix_4 = 6 + matrix_1
-# matrix_4 = matrix_1 + 6
-#
-# expand your solution to include other operations like
-# - subtraction 
-# - inversion
-# - string representation 
-#
-# Try to expand your implementation as best as you can.
-# Think of as many features as you can, and try implementing them.
-# Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-# Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-# The goal of this task is for you to SHOW YOUR BEST python programming skills.
-# Impress everyone with your skills, show off with your code.
-#
-# Your program must be runnable with command "python task.py".
-# Show some usecases of your library in the code (print some things)
-# Delete these comments before commit!
-#
-# Good luck.
---
+++ task_test.py
@@ -0,0 +1,94 @@
+from task import Matrix
+import numpy as np
+import unittest
+
+
+class TaskTest(unittest.TestCase):
+    def setUp(self) -> None:
+        data1 = np.random.randint(1, 10, size=(4, 4))
+        data2 = np.random.randint(1, 10, size=(4, 4))
+        self.matrix1 = Matrix(data1)
+        self.matrix2 = Matrix(data2)
+        self.np_matrix1 = np.array(data1)
+        self.np_matrix2 = np.array(data2)
+
+    def test_add_matrix(self):
+        res = self.matrix1 + self.matrix2
+        np_res = self.np_matrix1 + self.np_matrix2
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_add_number(self):
+        number = np.random.randint(10)
+        res = self.matrix1 + number
+        np_res = self.np_matrix1 + number
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_add_number_rev(self):
+        number = np.random.randint(10)
+        res = number + self.matrix1
+        np_res = number + self.np_matrix1
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    #
+
+    def test_sub_matrix(self):
+        res = self.matrix1 - self.matrix2
+        np_res = self.np_matrix1 - self.np_matrix2
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_sub_number(self):
+        number = np.random.randint(10)
+        res = self.matrix1 - number
+        np_res = self.np_matrix1 - number
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_sub_number_rev(self):
+        number = np.random.randint(10)
+        res = number - self.matrix1
+        np_res = number - self.np_matrix1
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    #
+
+    def test_mul_matrix(self):
+        res = self.matrix1 * self.matrix2
+        np_res = self.np_matrix1 * self.np_matrix2
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_mul_number(self):
+        number = np.random.randint(10)
+        res = self.matrix1 * number
+        np_res = self.np_matrix1 * number
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_mul_number_rev(self):
+        number = np.random.randint(10)
+        res = number * self.matrix1
+        np_res = number * self.np_matrix1
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_div_matrix(self):
+        res = self.matrix1 / self.matrix2
+        np_res = self.np_matrix1 / self.np_matrix2
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_div_number(self):
+        number = np.random.randint(1, 10)
+        res = self.matrix1 / number
+        np_res = self.np_matrix1 / number
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_div_number_rev(self):
+        number = np.random.randint(10)
+        res = number / self.matrix1
+        np_res = number / self.np_matrix1
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+    def test_mat_mul(self):
+        res = self.matrix1 @ self.matrix2
+        np_res = np.dot(self.np_matrix1, self.np_matrix2)
+        self.assertCountEqual(res.rows, np_res.tolist())
+
+
+if __name__ == '__main__':
+    unittest.main()
