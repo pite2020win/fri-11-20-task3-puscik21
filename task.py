d5b5754b9bfe483a8f3b06a57902290da114a75f
grzegorz.puscizna <grzesiekpuscizna@gmail.com>
2020-11-19 20:14:59 +0100

    Changed constructor (works as in numpy array)

--- task.py
+++ task.py
@@ -1,38 +1,54 @@
+import numpy as np
+
+
 class Matrix:
     def __init__(self, *args):
         self.rows = []
-        for row in args:
-            self.rows.append(row.numbers)
+        for row in list(*args):
+            self.rows.append(row)
 
     def __add__(self, other):
-        new_matrix = []
+        new_matrix = Matrix()
         for i in range(len(self.rows)):
-            new_matrix.append(self.rows[i] + other.rows[i])
+            row = []
+            for j in range(len(self.rows[i])):
+                row.append(self.rows[i][j] + other.rows[i][j])
+            new_matrix.rows.append(row)
         return new_matrix
 
     def __repr__(self):
         return "\n".join([str(row) for row in self.rows])
 
 
-class Row:
-    def __init__(self, *args):
-        self.numbers = args
+def print_matrix(name, matrix):
+    print("\n*** {} ***\n{}".format(name, matrix))
 
 
 if __name__ == '__main__':
-    row1 = Row(1, 2, 3, 4)
-    row2 = Row(1, 2, 3, 4)
-    row3 = Row(1, 2, 3, 4)
-    row4 = Row(1, 2, 3, 4)
-    matrix1 = Matrix(row1, row2, row3, row4)
-    matrix2 = Matrix(row1, row2, row3, row4)
+    matrix1 = Matrix([[1, 2],
+                      [3, 4]])
+    matrix2 = Matrix([[2, 4],
+                     [6, 8]])
 
-    matrix1 = matrix1 + matrix2
-    print(matrix1)
+    print_matrix("matrix1", matrix1)
+    print_matrix("matrix2", matrix2)
 
+    matrix3 = matrix1 + matrix2
+    print_matrix("matrix3", matrix3)
 
+# @ - dot product
 
-
+# arr1 = np.array([[1, 2],
+#                  [3, 4]])
+# arr2 = np.array([[2, 4],
+#                  [6, 8]])
+#
+# arr3 = arr1 * arr2
+# arr4 = np.dot(arr1, arr2)
+# print(arr1)
+# print(arr2)
+# print("arr3:\n{}".format(arr3))
+# print("arr4:\n{}".format(arr4))
 
 # Matrix.
 
